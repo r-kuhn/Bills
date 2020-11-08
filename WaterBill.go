b9aa72224df8f70c3494216e05d31f35722ae900
Robbie Kuhh <robbie.kuhn@outlook.com>
2020-11-07 20:54:38 -0500

    Cleaning up main func
    Fixes #4
    Fixes #5
    
    Cleaning up main func to have less code and call more functions. Final print
    statement now prints a more clear and concise list of payments per
    person.

--- WaterBill.go
+++ WaterBill.go
@@ -12,7 +12,6 @@ type Bills struct {
 }
 
 func main() {
-
 	b := Bills{
 		FixedCosts:    0.0,
 		VariableCosts: 0.0,
@@ -23,30 +22,21 @@ func main() {
 	///Map of each person on the water bill
 	guys := map[string]float32{"Robbie": 0.0, "Jackson": 0.0, "Chris": 0.0, "Daniel": 0.0}
 
-	fmt.Println("Enter the total fixed cost: ")
-	// Takes the fixed costs from user input
-	fmt.Scanln(&b.FixedCosts)
+	b = GetData(b)
 
-	fmt.Println("Enter the total variable cost: ")
-	// Takes the total variable cost from the users input
-	fmt.Scanln(&b.VariableCosts)
-
-	fmt.Println("Enter the total number of billing days: ")
-	// Takes the total number of billing period days from the user
-	fmt.Scanln(&b.NumDays)
-
-	fmt.Println("Enter the number of days Robbie was there: ")
-	// Takes the number of days Robbie was at the house from the user
-	fmt.Scanln(&b.NumRobbieDays)
-
-	//Calculates the fixed cost per person and returns each persons value.
 	FixedCostPerPerson(b.FixedCosts, 4, guys)
+
 	VariableCostPerPerson(b.VariableCosts, b.NumDays, b.NumRobbieDays, guys)
-	fmt.Println(guys)
+
+	fmt.Println(
+		"Robbie: $", guys["Robbie"],
+		"\nJackson: $", guys["Jackson"],
+		"\nChris: $", guys["Chris"],
+		"\nDaniel: $", guys["Daniel"])
 
 }
 
-//FixedCostPerPerson prints the total fixed cost
+//FixedCostPerPerson calculates the amount each person pays of the fixed cost
 func FixedCostPerPerson(fixed float32, NumPeople float32, guys map[string]float32) map[string]float32 {
 	fmt.Println("Total Fixed cost is: $", fixed)
 	guys["Robbie"] = fixed / NumPeople
@@ -56,8 +46,9 @@ func FixedCostPerPerson(fixed float32, NumPeople float32, guys map[string]float3
 	return guys
 }
 
-//VariableCostPerPerson calculates the amount each person pays of the variable water costs based on the amount of time spent at the house.
+//VariableCostPerPerson calculates the amount each person pays of the variable water costs based on the amount of time spent at the house
 func VariableCostPerPerson(VariableCost float32, NumDays float32, NumRobbieDays float32, guys map[string]float32) map[string]float32 {
+	fmt.Println("Total Variable Cost is: $", VariableCost)
 	ChargePerDay := VariableCost / NumDays
 	PricePerDayRobbieThere := (ChargePerDay * NumRobbieDays) / 3
 	PricePerDayRobbieNotThere := (ChargePerDay * (NumDays - NumRobbieDays)) / 2
@@ -67,4 +58,25 @@ func VariableCostPerPerson(VariableCost float32, NumDays float32, NumRobbieDays 
 	guys["Chris"] = guys["Chris"] + PricePerDayRobbieThere + PricePerDayRobbieNotThere
 
 	return guys
+}
+
+//GetData gets the data from user input and assigns to the fields of Bills struct
+func GetData(bills Bills) Bills {
+	fmt.Println("Enter the total fixed cost: ")
+	// Takes the fixed costs from user input
+	fmt.Scanln(&bills.FixedCosts)
+
+	fmt.Println("Enter the total variable cost: ")
+	// Takes the total variable cost from the users input
+	fmt.Scanln(&bills.VariableCosts)
+
+	fmt.Println("Enter the total number of billing days: ")
+	// Takes the total number of billing period days from the user
+	fmt.Scanln(&bills.NumDays)
+
+	fmt.Println("Enter the number of days Robbie was there: ")
+	// Takes the number of days Robbie was at the house from the user
+	fmt.Scanln(&bills.NumRobbieDays)
+
+	return bills
 }
